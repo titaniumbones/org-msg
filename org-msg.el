c62a55e18369dad6d6ed4a09f22c4f2702f4bea9
Matt Price <matt.price@utoronto.ca>
2019-09-03 13:31:04 -0400

    this is working for me sept 3 2019

--- org-msg.el
+++ org-msg.el
@@ -589,7 +589,8 @@ absolute paths."
 
 (defun org-msg-build ()
   "Build and return the XML tree for current OrgMsg buffer."
-  (let ((css (org-msg-load-css)))
+  (let ((css (org-msg-load-css))
+        (reply-element (if org-html-html5-fancy 'article 'div)))
     (cl-flet ((enforce (xml)
 	       (let* ((tag (car xml))
 		      (tmp (assq 'class (cadr xml)))
@@ -616,8 +617,8 @@ absolute paths."
 	  (org-msg-xml-walk (assq 'body reply) #'enforce))
 	(if (not original)
 	    (assq-delete-all 'script (assq 'head reply))
-	  (org-msg-improve-reply-header original css)
-	  (push (assq 'div (assq 'body reply)) (cddr (assq 'body original))))
+	  ;;(org-msg-improve-reply-header original css)
+	  (push (assq reply-element (assq 'body reply)) (cddr (assq 'body original))))
 	(or original reply)))))
 
 (defun org-msg-preview (arg)
